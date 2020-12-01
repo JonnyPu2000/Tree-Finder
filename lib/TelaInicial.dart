0e303de865b53f9d570a52c9b9ab8def5d60503e
JonnyPu2000 <57176998+JonnyPu2000@users.noreply.github.com>
2020-12-01 15:58:04 -0300

    Tela Inicial OK

---
+++ lib/TelaInicial.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TelaInicial(),
+  ));
+}
+
+class TelaInicial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/Fundo.jpg"),
+                      fit: BoxFit.cover))),
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Colors.transparent,
+              Colors.transparent,
+              Colors.black87.withOpacity(0.4),
+              Colors.black87.withOpacity(0.6),
+              Colors.black87.withOpacity(0.85),
+              Colors.black.withOpacity(0.85)
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 300,
+                ),
+                Text(
+                  "Bem Vindo!",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 38,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Informe seu Nome",
+                  style: TextStyle(color: Colors.grey.shade500, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 60,
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  child: TextField(
+                    style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
+                    decoration: InputDecoration(
+                      hintText: "Nome",
+                      hintStyle: TextStyle(color: Colors.grey.shade700),
+                      filled: true,
+                      fillColor: Colors.black87.withOpacity(0.9),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent[400])),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent[400])),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  height: 60,
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: "Sobrenome",
+                      hintStyle: TextStyle(color: Colors.grey.shade700),
+                      filled: true,
+                      fillColor: Colors.black87.withOpacity(0.9),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent[400])),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent[400])),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 60,
+                  width: double.infinity,
+                  margin: EdgeInsets.only(left: 90, right: 90),
+                  child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      color: Colors.greenAccent,
+                      onPressed: () {},
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      )),
+                ),
+                SizedBox(height: 30)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
